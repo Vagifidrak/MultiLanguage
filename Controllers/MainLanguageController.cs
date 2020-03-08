d85e47f955be2fdfb927752cb241b696e0876001
VagifIdrak <vaqa.ibrahim@gmail.com>
2020-03-08 23:08:36 +0400

    MultiLanguage

---
+++ Controllers/MainLanguageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Multilang.Controllers
+{
+    public class MainLanguageController : Controller
+    {
+        // GET: MainLanguage
+        public ActionResult LangSetting(string lang)
+        {
+            if (lang != "") {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(lang);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(lang);
+            }
+            else
+            {
+                Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture("az");
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("az");
+            }
+            Response.Cookies.Add(new HttpCookie("coRes")
+            {
+                Value = lang
+            });
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ MultiExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multilang
+{
+    public static class MultiExtension
+    {
+        public static string Language(this HttpCookieCollection cookie)
+        {
+            return cookie["coRes"]?.Value;
+        }
+    }
+}
